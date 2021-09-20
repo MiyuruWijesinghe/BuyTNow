78e003ed4e071ecb53e4cb544297560b4047d00b
MiyuruWijesinghe <mkwijesinghe2016@gmail.com>
2021-09-20 20:38:35 +0530

    added initial project

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>BuyTNow</Text>
+    </View>
+  )
+}
+
+export default App;
